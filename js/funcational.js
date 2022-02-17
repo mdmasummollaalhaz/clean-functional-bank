68315af7ee990c000e96c0be5e7177acd7b22106
Masum Mollo <freelancermasummolla@gmail.com>
2022-02-17 22:51:09 +0600

    deposit, withdraw, balance

--- js/funcational.js
+++ js/funcational.js
@@ -14,9 +14,31 @@ function updateTotal(fieldId, amount){
     totalTag.innerText = newTotal;
 }
 
+function updateBalance(amount, isAdding){
+    const balanceTag = document.getElementById('balance-total');
+    const balanceInText = balanceTag.innerText;
+    const previousBalance = parseFloat(balanceInText);
+    let newBalance;
+    if(isAdding == true){
+        newBalance = previousBalance + amount;
+    }
+    else{
+        newBalance = previousBalance - amount;
+    }
+    balanceTag.innerText = newBalance;
+
+}
+
 document.getElementById('deposit-btn').addEventListener('click', function(){
     // console.log('clicked');
     const amount = getInputValue('deposit-input');
     updateTotal('deposit-total', amount);
-    
+    updateBalance(amount, true);
 })
+
+// Handle withdraw
+document.getElementById('withdraw-btn').addEventListener('click', function(){
+    const amount = getInputValue('withdraw-input');
+    updateTotal('withdraw-total', amount);
+    updateBalance(amount,  false);
+});
